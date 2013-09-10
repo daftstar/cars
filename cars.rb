3508327b991a2af6b6e94006377f9169cab6488e
Nik Daftary <nikdaftary@gmail.com>
2013-09-10 15:55:42 -0500

    allows for convertibles through convertible class

--- cars.rb
+++ cars.rb
@@ -25,21 +25,23 @@ class Car
 
 	def self.colors_per_car #this allows us to call the class variable @@total_car_count
 		
-		puts "you have #{@@colors_per_car} cars" #implied return
+		#puts "you have #{@@colors_per_car} cars"
+		@@colors_per_car
 	end
 
 	def self.most_popular_color
-		colors_by_popularity = @@colors_per_car.sort_by { |color, amount| amount }
-		most_pop = colors_by_popularity[-1]
+		@colors_by_popularity = @@colors_per_car.sort_by { |color, amount| amount }
+		most_pop_array = @colors_by_popularity[-1]
+		most_pop_array[0]
 	end
 
+	def self.second_best
+		second_best = @colors_by_popularity[-2][0]
+	end
 
 	## ------------ INSTANCE METHODS -----------------------------------------
 
-	def change_car_color(old_color,new_color)
-		@@colors_per_car[old_color] -= 1 
-		@@colors_per_car[new_color] += 1 
-	end
+
 
 
 
@@ -65,30 +67,76 @@ class Car
 		puts "You must pay $#{3.5 * gallons_needed}"
 		@fuel = 10.0
 	end
+
+	def color=(input)
+		@@colors_per_car[@color] -= 1
+		@@colors_per_car[input] += 1
+		@color = input
+	end
+	# def color replaces the below code: 
+	# 	def change_car_color(old_color,new_color)
+	# 	@@colors_per_car[old_color] -= 1 
+	# 	@@colors_per_car[new_color] += 1 
+	# 	end
+
+
+end #end car class
+
+class Convertible < Car
+	attr_accessor :roof_status
+
+	def top_down
+		@roof_status = "down"
+		puts "Feel the breeze you sexy hero."
+	end
+
+
+	def top_up
+		@roof_status = "up"
+		puts "shit..it's raining huh?"
+	end
+
 end
+
+
 #--------------------------
 
 
 
-bluecar1 = Car.new("blue")
-bluecar2 = Car.new("blue")
+# bluecar1 = Car.new("blue")
+# bluecaraa = Car.new("blue")
+# bluecar2 = Car.new("blue")
+# bluecar3 = Car.new("blue")
+bluecar4 = Car.new("blue")
 redcar = Car.new("red")
+redcar2 = Car.new("red")
 greencar = Car.new("green")
 nastycar = Car.new("yellow")
-indycar = Car.new("white")
+# indycar = Car.new("white")
+# bluecar1.color = "red"
 
+# puts Car.colors_per_car
+# puts""
+# puts Car.most_popular_color
+
+# puts "now we change car colors and update the count"
+# puts ""
+
+# nastycar.color=("metallic")
+# indycar.color=("puke yellow")
+# puts "-----"
+# puts Car.colors_per_car
+# puts""
+# puts Car.most_popular_color
+
+# puts  Car.second_best
+puts Car.total_car_count
+
+porche = Convertible.new("silver")
 
 puts Car.colors_per_car
-puts""
+puts " "
 puts Car.most_popular_color
-
-nastycar.change_car_color("yellow","red")
-indycar.change_car_color("yellow","red")
-puts "-----"
-puts Car.colors_per_car
-puts""
-puts Car.most_popular_color
-
 
 # redcar1 = Car.new(:red)
 # redcar2 = Car.new(:red)
@@ -110,7 +158,12 @@ puts Car.most_popular_color
 # puts car_a
 # puts car_b
 # puts "---------------"
-# puts Car.total_car_count
+puts Car.total_car_count
+porche.top_down
+porche.top_up
+
+
+
 #puts car_b
 
 # #puts car_b.drive(1)
